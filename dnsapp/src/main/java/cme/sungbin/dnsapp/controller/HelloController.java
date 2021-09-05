0f08e405bad0da49e18dbdbfec090c5bc5295f3b
Robert.Yang <ysb05222@naver.com>
2021-09-06 05:48:39 +0900

    DNS 실습 및 host 설명
    DNS 실습 및 host 설명

---
+++ dnsapp/src/main/java/cme/sungbin/dnsapp/controller/HelloController.java
@@ -0,0 +1,13 @@
+package cme.sungbin.dnsapp.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "long time no see";
+    }
+}
