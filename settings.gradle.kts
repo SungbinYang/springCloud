dafd13fa062522e5747cfaae097107465845bd79
Robert.Yang <ysb05222@naver.com>
2021-09-08 05:43:09 +0900

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -7,7 +7,7 @@
  * in the user manual at https://docs.gradle.org/6.7.1/userguide/multi_project_builds.html
  */
 
-rootProject.name = "fastcampus-spring-cloud"
+rootProject.name = "spring-cloud"
 
-include("fastcam-photoapp")
+include("photoapp")
 
