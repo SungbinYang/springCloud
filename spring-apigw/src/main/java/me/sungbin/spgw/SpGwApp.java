f2aa40acf8704b054c62836c277dce2e9b8212d2
Robert.Yang <ysb05222@naver.com>
2021-09-11 19:25:36 +0900

    eurkea 활용
    eurkea 활용

--- spring-apigw/src/main/java/me/sungbin/spgw/SpGwApp.java
+++ spring-apigw/src/main/java/me/sungbin/spgw/SpGwApp.java
@@ -2,9 +2,11 @@ package me.sungbin.spgw;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 //@EnableEurekaClient
-//@EnableDiscoveryClient
+@EnableDiscoveryClient
 @SpringBootApplication
 public class SpGwApp {
     public static void main(String[] args) {
