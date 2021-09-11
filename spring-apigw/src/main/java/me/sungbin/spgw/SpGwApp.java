7465ae8ed6c2b316b09fc48056c6fcaf5c2affd4
Robert.Yang <ysb05222@naver.com>
2021-09-11 14:25:03 +0900

    spring cloud gateway 활용
    spring cloud gateway 활용

---
+++ spring-apigw/src/main/java/me/sungbin/spgw/SpGwApp.java
@@ -0,0 +1,13 @@
+package me.sungbin.spgw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+//@EnableEurekaClient
+//@EnableDiscoveryClient
+@SpringBootApplication
+public class SpGwApp {
+    public static void main(String[] args) {
+        SpringApplication.run(SpGwApp.class, args);
+    }
+}
