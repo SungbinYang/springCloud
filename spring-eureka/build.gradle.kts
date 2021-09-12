e1481a3c7b371d450f925a0b9150560d91492d0d
Robert.Yang <ysb05222@naver.com>
2021-09-12 23:46:17 +0900

    FeignClient와 eurkea 연동
    FeignClient와 eurkea 연동

--- spring-eureka/build.gradle.kts
+++ spring-eureka/build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 	kotlin("plugin.spring") version "1.4.30"
 }
 
-group = "me.sungbin.eureka"
+group = "me.sungbin.springCloud"
 version = "0.0.1-SNAPSHOT"
 java.sourceCompatibility = JavaVersion.VERSION_11
 
